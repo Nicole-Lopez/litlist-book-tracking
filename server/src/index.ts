aec7e2efcc1ac512984ce31a319abad42ab97b78
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-08-26 03:43:56 -0300

    Configure Apollo Server on Express route /graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
 import { readFileSync } from 'fs'
 import dotenv from 'dotenv'
 import resolvers from '@resolvers/index.js'
 import { BestSellersAPI } from '@dataSources/BestSellersAPI.js'
 import { BooksAPI } from '@dataSources/BooksAPI.js'
 import { CategoriesAPI } from '@dataSources/CategoriesAPI.js'
+
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import pkg from 'body-parser'
+const { json } = pkg
 
 const typeDefs = readFileSync('src/schema/schema.graphql', {
 	encoding: 'utf-8',
@@ -20,24 +27,37 @@ export interface ContextServer {
 	}
 }
 
+const app = express()
+const httpServer = http.createServer(app)
 const server = new ApolloServer<ContextServer>({
 	typeDefs,
 	resolvers,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
 
-const { url } = await startStandaloneServer(server, {
-	context: async () => {
-		const { cache } = server
-		return {
-			dataSources: {
-				bestSellersAPI: new BestSellersAPI({ cache }),
-				booksAPI: new BooksAPI({ cache }),
-				categoriesAPI: new CategoriesAPI({ cache }),
-			},
-		}
-	},
+await server.start()
 
-	listen: { port: Number(process.env.PORT) },
-})
+app.use(
+	'/graphql',
+	cors<cors.CorsRequest>(),
+	json(),
+	expressMiddleware(server, {
+		context: async () => {
+			const { cache } = server
+			return {
+				dataSources: {
+					bestSellersAPI: new BestSellersAPI({ cache }),
+					booksAPI: new BooksAPI({ cache }),
+					categoriesAPI: new CategoriesAPI({ cache }),
+				},
+			}
+		},
+	}),
+)
 
-console.log(`Server ready at: ${url}`)
+await new Promise<void>(resolve =>
+	httpServer.listen({ port: Number(process.env.PORT) }, resolve),
+)
+console.log(
+	`Server ready at: http://localhost:${Number(process.env.PORT)}/graphql`,
+)
