e247f8890ae19dbcad7c1aa9cbbefe843008ffd8
Nicole-Lopez <ananicolelopez96@gmail.com>
2026-03-03 05:10:12 -0300

    chore(client): upgrade vite and update configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
 		alias: {
-			'@adapters': path.resolve(__dirname, './src/adapters'),
-			'@assets': path.resolve(__dirname, './src/assets'),
-			'@components': path.resolve(__dirname, './src/components'),
-			'@constants': path.resolve(__dirname, './src/constants'),
-			'@contexts': path.resolve(__dirname, './src/contexts'),
-			'@styles': path.resolve(__dirname, './src/styles'),
-			'@hooks': path.resolve(__dirname, './src/hooks'),
-			'@models': path.resolve(__dirname, './src/models'),
-			'@types': path.resolve(__dirname, './src/types'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@router': path.resolve(__dirname, './src/router'),
-			'@services': path.resolve(__dirname, './src/services'),
-			'@utilities': path.resolve(__dirname, './src/utilities'),
+			'@adapters': fileURLToPath(new URL('./src/adapters', import.meta.url)),
+			'@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+			'@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+			'@constants': fileURLToPath(new URL('./src/constants', import.meta.url)),
+			'@contexts': fileURLToPath(new URL('./src/contexts', import.meta.url)),
+			'@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+			'@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+			'@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+			'@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+			'@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+			'@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+			'@services': fileURLToPath(new URL('./src/services', import.meta.url)),
+			'@utilities': fileURLToPath(new URL('./src/utilities', import.meta.url)),
 		},
 	},
 	plugins: [react()],
