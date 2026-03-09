d7e7ee8b8909b87ce4f8ec752fd105701999e51e
Nicole-Lopez <ananicolelopez96@gmail.com>
2026-03-09 07:27:46 -0300

    chore(client/lint): update stylelint rules

--- client/stylelint.config.mjs
+++ client/stylelint.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import("stylelint").Config} */
 export default {
-    extends: ['stylelint-config-standard-scss'],
-    rules: {
-        'no-descending-specificity': null,
-    },
+	extends: ['stylelint-config-standard-scss'],
+	rules: {
+		'no-descending-specificity': null,
+		'selector-class-pattern': null,
+		'scss/at-mixin-argumentless-call-parentheses': 'always',
+	},
 }
