dc50acb65d3e4e433ed84ef1ee3816c34e135991
Nicole-Lopez <ananicolelopez96@gmail.com>
2026-06-29 03:19:22 -0300

    feat(client): add BookSummary and BookPreview models

---
+++ client/src/models/book.models.ts
@@ -0,0 +1,27 @@
+import type { Prettify } from '@customTypes/customUtilityTypes'
+
+export type BookId = string
+export type BookAuthor = string
+export type BookCategory = string
+export type BookPageCount = number
+export type BookContentWarning = string
+export type BookPublishedYear = number
+
+export type BookSummary = {
+	id: BookId
+	isExternalId: boolean
+	title: string
+	authors?: BookAuthor[]
+	cover?: string
+	isbn10?: string
+	isbn13?: string
+}
+
+export type BookPreview = Prettify<
+	BookSummary & {
+		publishedYear?: BookPublishedYear
+		pageCount?: BookPageCount
+		categories?: BookCategory[]
+		contentWarnings?: BookContentWarning[]
+	}
+>
